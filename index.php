5aa825ae3716d3dbfbd813069f4a0cd55193206e
yatan <yatan87@gmail.com>
2018-01-01 19:48:08 +0100

    Show all data, index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+	include("connect.php");
+	
+	$mysqli = new mysqli($server, $user, $pass, $db);
+	
+	if ($mysqli->connect_errno) 
+	{
+		echo "Error: Fallo al conectarse a MySQL debido a: \n";
+		echo "Errno: " . $mysqli->connect_errno . "\n";
+		echo "Error: " . $mysqli->connect_error . "\n";
+		exit;
+	}
+
+	
+	$sql = "SELECT data, temp FROM temp_data";
+	$result = $mysqli->query($sql);
+
+	if ($result->num_rows > 0) {
+		// output data of each row
+		while($row = $result->fetch_assoc()) {
+			echo "Data: " . $row["data"]. " - Temperatura: " . $row["temp"]. "<br>";
+		}
+	} else {
+		echo "0 results";
+	}
+	$mysqli->close();
+
+?>
